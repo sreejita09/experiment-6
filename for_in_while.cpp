7632035972376a848c7ebaff471dbbc4d7e0c2db
Sreejita Bhardwaj <sreejitabhardwaj2005@gmail.com>
2024-08-01 22:47:07 +0530

    Add files via upload

---
+++ for_in_while.cpp
@@ -0,0 +1,33 @@
+//Sreejita Bhardwaj 
+//Prn: 23070123130 
+//experiment 6: loops
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int rows, columns, i = 1;
+    char ch;
+
+    cout << "number of rows: ";
+    cin >> rows;
+
+    cout << "number of columns: ";
+    cin >> columns;
+
+    cout << "character: ";
+    cin >> ch;
+
+    while (i <= rows) 
+    {
+        for (int j = 1; j <= columns; j++) 
+        {
+            cout << ch << " ";
+        }
+        cout <<endl;
+        i++;
+    }
+
+    return 0;
+}
